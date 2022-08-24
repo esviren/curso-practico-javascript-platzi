aa76ea264fa7e9ff0f75f8ac3bae127786d260fd
esvirenson <st-rodriguez@hotmail.com>
2022-08-24 15:25:54 -0500

    Promeras clases, creacion de branch steevens

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting(){
+    var userName = 'Jessy';
+    console.log(userName);
+
+    if (userName === 'Jessy') {
+        console.log('I love you ' + userName);
+    }    
+}
+greeting();
+
+
+console.log(userName);
---
+++ src/scope/global.js
@@ -0,0 +1,18 @@
+//variables
+
+var a; //delaramos
+var b = 'b'; //declarar y asignar
+b = 'bb'; // dedeclaración
+var a = 'aa'; //reasignación
+
+
+var fruit = 'Apple';
+console.log(fruit);
+
+function bestFruit(){
+    
+    console.log(fruit);
+}
+
+bestFruit();
+bestFruit();
